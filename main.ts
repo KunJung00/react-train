ad1b747bfed54efa50bef09bad0f5a286370ccd7
Gongjung <64011212101@msu.ac.th>
2024-10-29 15:30:16 +0700

    data type

--- main.ts
+++ main.ts
@@ -1,10 +1,47 @@
-import thistIsDefault, { anotherText, myText } from "./utils";
-//thisIsDefault เป็นค่าเริ่มต้นที่ตั้งไว้จาก class อื่น โดย type data จะเป็น default
+// import thistIsDefault, { anotherText, myText } from "./utils";
+//===================================================================================== export import
+// //thisIsDefault เป็นค่าเริ่มต้นที่ตั้งไว้จาก class อื่น โดย type data จะเป็น default
 
-console.log('Hello jung');
-console.log(myText);
-console.log(anotherText);
-console.log(thistIsDefault);
+// console.log('Hello jung');
+// console.log(myText);
+// console.log(anotherText);
+// console.log(thistIsDefault);
+//====================================================================================== data type
+// let tx1: string = 'hello';
+// // บังคับ string 
+// let tx2 = 'hello';
+// // 
+// let tx3: any = 'hello';
+// // คือ type อะไรก็ได้
+
+// console.log(typeof (tx1));
+// console.log(typeof (tx2));
+// console.log(typeof (tx3));
+
+// // tx1 = 10; // มันบังคับ string
+// // tx2 = 10;// type เริ่มต้นเป็น string
+// tx3 = 10;// type เป็นอะไรก็ได้ไม่ว่าค่าก่อนหน้าจะเป็นอะไร
+
+// console.log(tx1);
+// console.log(tx2);
+// console.log(tx3);
+// console.log(typeof (tx1));
+// console.log(typeof (tx2));
+// console.log(typeof (tx3));
+//-----------------underfined Null
+let u = undefined;//ควรใส่ค่าแต่ไม่มีค่า
+console.log(u);
+u = true;
+console.log(u);
+console.log(typeof u);
+
+let n = null;//มีค่าเป็น null คือไม่มีค่า
+console.log(n);
+n = true;
+console.log(n);
+console.log(typeof n);
+
+
 
 
 
