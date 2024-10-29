05e732098df3160f0756e35636e4a71d49ea273c
Gongjung <64011212101@msu.ac.th>
2024-10-29 15:01:39 +0700

    first commit

---
+++ main.ts
@@ -0,0 +1,6 @@
+import { myText } from "./utils";
+
+console.log('Hello jung');
+console.log(myText);
+
+
---
+++ utils.ts
@@ -0,0 +1,3 @@
+export const myText = 'Hello from Utils';// ใช่ export ในการส่งข้อมูลข้ามหน้า
+export let anotherText = 'i am utils'
+export default 'a Default '
