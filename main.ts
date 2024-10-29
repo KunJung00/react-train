730e341350ef4375077794c6da1c894426e82be7
Gongjung <64011212101@msu.ac.th>
2024-10-29 15:13:00 +0700

    type data

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-import { myText } from "./utils";
+import thistIsDefault, { anotherText, myText } from "./utils";
+//thisIsDefault เป็นค่าเริ่มต้นที่ตั้งไว้จาก class อื่น โดย type data จะเป็น default
 
 console.log('Hello jung');
 console.log(myText);
+console.log(anotherText);
+console.log(thistIsDefault);
+
 
 
